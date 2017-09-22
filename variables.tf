08d70412dc6ee939f5c6a6fffa55f9da62baae20
john.george <john.george@claranet.uk>
2017-09-22 11:39:24 +0100

    Added description, defaults and type to variables

--- variables.tf
+++ variables.tf
@@ -1,12 +1,58 @@
-variable "es_domain_envname" {}
-variable "es_version" {}
-variable "es_ebs_size" {}
-variable "es_node_instance_type" {}
-variable "es_node_instance_count" {}
-variable "es_dedicated_master" {
-  default = "false"
+variable "es_domain_envname" {
+  description = "The desired name for the ElasticSearch domain"
+  type = "string"
 }
-variable "es_master_instance_type" {}
-variable "es_master_instance_count" {}
-variable "es_zone_awareness" {}
-variable "aws_region" {}
+
+variable "es_version" {
+  description = "The version of ElasticSearch to deploy"
+  type = "string"
+  default = "1.5"
+}
+
+variable "es_ebs_size" {
+  description = "The size (GB) of EBS volumes attached to data nodes"
+  type = "string"
+  default = 50
+}
+
+variable "es_node_instance_type" {
+  description = "Instance type of data nodes in the cluster"
+  type = "string"
+  default = "t2.micro.elasticsearch"
+}
+
+variable "es_node_instance_count" {
+  description = "Number of instances in the cluster"
+  type = "string"
+  default = 2
+}
+
+variable "es_dedicated_master" {
+  description = "Bool indicating whether dedicated master nodes are enabled for the cluster"
+  type = "string"
+  default = false
+}
+
+variable "es_master_instance_type" {
+  description = "Instance type of the dedicated master nodes in the cluster"
+  type = "string"
+  default = "t2.micro.elasticsearch"
+}
+
+variable "es_master_instance_count" {
+  description = "Number of dedicated master nodes in the cluster"
+  type = "string"
+  default = 1
+}
+
+variable "es_zone_awareness" {
+  description = "Bit indicating whether zone awareness is enabled"
+  type = "string"
+  default = 1
+}
+
+variable "aws_region" {
+  description = "The AWS region in which to deploy resources"
+  type = "string"
+  default = "eu-west-1"
+}
